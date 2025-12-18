b77b5e3db6b0ef7f8029edf01028d0336103814d
nlechner <nicolas.lechner08@htl-salzburg.ac.at>
2025-12-18 09:47:11 +0100

    Spieler Movements

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,26 +4,45 @@ public class Movement : MonoBehaviour
 {
     public Rigidbody2D rB;
     public string rightKey = "D";
-
-    void Start()
-    {
-
-    }
+    bool isTouching_ground = false;
+    public int jump_force = 300;
 
     void Update()
     {
         if (Input.GetKey(KeyCode.D))
         {
-            rB.AddForceX(7);
+            rB.AddForce(Vector2.right * 7);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            rB.AddForceX(-7);
+            rB.AddForce(Vector2.left * 7);
         }
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isTouching_ground)
         {
-            rB.AddForceY(25);
+            rB.AddForceY(jump_force);
         }
     }
 
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+
+            foreach (ContactPoint2D contact in collision.contacts)
+            {
+                if (contact.normal.y > 0.5f) 
+                {
+                    isTouching_ground = true;
+                }
+            }
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isTouching_ground = false;
+        }
+    }
 }
