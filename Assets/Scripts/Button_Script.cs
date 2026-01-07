c1d85026da6d951d80a65b3a8617cd35684f82eb
tobidobi26 <wallingertobias60@gmail.com>
2026-01-07 10:29:48 +0100

    fertigstellen des start-screens

--- Assets/Scripts/Button_Script.cs
+++ Assets/Scripts/Button_Script.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Button_Script : MonoBehaviour
 {
-       public void ausgabe()
-       {
-             Debug.Log("Hat funktioniert");
-       }
+      public string Scene = "start_scene";
+      public void switch_scene(){
+      SceneManager.LoadScene(Scene);
+      }
+      public void quit(){
+            Application.Quit();
+      }
 }
